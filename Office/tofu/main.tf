4f8b2fcea9e996bae61a7048362bec5c58fab625
rohit roshan jena <rohit.r@iserveu.co.in>
2025-06-04 13:14:43 +0530

    Comment out Kubernetes deployment module for clarity and future reference

--- Office/tofu/main.tf
+++ Office/tofu/main.tf
@@ -49,15 +49,15 @@
   # }
 
 
-  module "k8s_deploy" {
-    source           = "./modules/k8s_deploy"
-    app_name         = var.app_name
-    # image            = "${var.image}:latest"
-    # host             = minikube_cluster.cluster.host
-    # client_certificate = minikube_cluster.cluster.client_certificate
-    # client_key         = minikube_cluster.cluster.client_key
-    # cluster_ca_certificate = minikube_cluster.cluster.cluster_ca_certificate
-  }
+  # module "k8s_deploy" {
+  #   source           = "./modules/k8s_deploy"
+  #   app_name         = var.app_name
+  #   # image            = "${var.image}:latest"
+  #   # host             = minikube_cluster.cluster.host
+  #   # client_certificate = minikube_cluster.cluster.client_certificate
+  #   # client_key         = minikube_cluster.cluster.client_key
+  #   # cluster_ca_certificate = minikube_cluster.cluster.cluster_ca_certificate
+  # }
 
   module "vm" {
     source           = "./modules/vm"
