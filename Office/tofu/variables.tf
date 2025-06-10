64d0021c23c13bb8bd51324960c5d5264969f8c5
rohit roshan jena <rohit.r@iserveu.co.in>
2025-06-10 12:28:35 +0530

    Update app_name variable type from map to list in variables.tf

--- Office/tofu/variables.tf
+++ Office/tofu/variables.tf
@@ -1,6 +1,6 @@
 variable "app_name" {
   description = "Map of application names to container image paths"
-  type        = map(string)
+  type        = list(string)
   default = {
     tofuredicrect     = "gcr.io/iserveustaging/readytodeploy-redirect-app@sha256:c9b929a54ee055ee5b23bee229907c1b1e54b2a19db81a6fefaebacd4fd4b95a"
     tofuauth          = "gcr.io/iserveustaging/readytoddeploy-ext-authz@sha256:785cd3865f9dd9a188e31caa56d7a1f595b2cf5c3727afd2436dbb1aa79b34c6"
@@ -17,6 +17,7 @@ variable "app_name" {
   # }
 
   variable "grpcprovider" {
+    type = string
     default = "tofu-grpc-authprovider"
   }
 
